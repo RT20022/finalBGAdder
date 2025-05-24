bed91a7b59aa9bad82db3aba49269f25faf8f633
Raghav Trikha <rtrikha99@gmail.com>
2025-05-24 18:12:54 +0530

    updating the UI of Dashboard

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -4,70 +4,70 @@ import { cookies } from "next/headers"
 let repoName = ""
 
 async function GetData() {
-    let cookieStore = cookies();
-    let repoUrl = (await cookieStore).get("selectedRepo")
-    try {
-        const result = await fetch(`${repoUrl?.value}/Blogs`)
-        interface Group {
-            noOfBlogs : number,
-            allBlogArray : string[]
-        }
-        const dataObj: Group = {
-  noOfBlogs: 0,
-  allBlogArray: [],
-};
-        let noOfBlogs = 0
-        let allBlogArray : string[] = [];
-
-        const resp = await result.json()
-        resp.forEach((element : any) => {
-            if (element.name != "blogs.html" && element.name != "blog.css") {
-                noOfBlogs += 1
-                allBlogArray.push(element.name)
-            }
-        });
-
-        dataObj.noOfBlogs =noOfBlogs
-        dataObj.allBlogArray = allBlogArray
-
-        console.log(noOfBlogs, dataObj , allBlogArray)
-
-        return dataObj
-        
-    } catch (error) {
-        console.log(error, "Error Occured in Dashboard Page")
+  let cookieStore = cookies();
+  let repoUrl = (await cookieStore).get("selectedRepo")
+  try {
+    const result = await fetch(`${repoUrl?.value}/Blogs`)
+    interface Group {
+      noOfBlogs: number,
+      allBlogArray: string[]
     }
+    const dataObj: Group = {
+      noOfBlogs: 0,
+      allBlogArray: [],
+    };
+    let noOfBlogs = 0
+    let allBlogArray: string[] = [];
+
+    const resp = await result.json()
+    resp.forEach((element: any) => {
+      if (element.name != "blogs.html" && element.name != "blog.css") {
+        noOfBlogs += 1
+        allBlogArray.push(element.name)
+      }
+    });
+
+    dataObj.noOfBlogs = noOfBlogs
+    dataObj.allBlogArray = allBlogArray
+
+    console.log(noOfBlogs, dataObj, allBlogArray)
+
+    return dataObj
+
+  } catch (error) {
+    console.log(error, "Error Occured in Dashboard Page")
+  }
 }
 
 export default async function PAGE() {
-   let allData = await GetData();
-    if(allData){
-        return (
-        <>
-            <div className="flex">
-                <div className=" text-2xl   p-5 border-violet-700 border-4 border text-center">
-                    <h2>Connected Repo : 1</h2> 
-                </div>
-                <div className=" text-2xl p-5 border-4 border text-center ml-5">
-                    <h2>No of Blogs Added : {allData.noOfBlogs}</h2> 
-                    <h3></h3>
-                </div>
-            </div>
-                <div className=" text-2xl p-5 border-4 border text-center ml-5">
-                    <h2>Blogs Name :- </h2> 
-                    {allData.allBlogArray.map((val,key)=>{
-                        return (
-                            <h3 key={key}>{val}</h3>
-                        )
-                    })}
-                </div>
-        </>
+  let allData = await GetData();
+  if (allData) {
+    return (
+      <>
+        <div className="flex">
+          <div className=" text-2xl   p-5 border-violet-700 border-4 border text-center">
+            <h2>Connected Repo : 1</h2>
+          </div>
+          <div className=" text-2xl p-5 border-4 border text-center ml-5">
+            <h2>No of Blogs Added : {allData.noOfBlogs}</h2>
+            <h3></h3>
+          </div>
+        </div>
+        <div className=" text-2xl p-5 border-4 border text-center ml-5 d-block bg-cyan-500 ">
+          <h2>Blogs Name :- </h2>
+          {allData.allBlogArray.map((val, key) => {
+            return (
+              <h3 key={key}>{val}</h3>
+            )
+          })}
+        </div>
+      </>
     )
-    }
-    else{
-        return (
-            <>An error Occured while loading the page. Kindly Contact Admin.</>
-        )
-    }
+  }
+  else {
+    return (
+      <>An error Occured while loading the page. Kindly Contact Admin.</>
+    )
+  }
 }
 
